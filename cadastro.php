625038c21f145c1f288a111751903972b9ecfc88
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2021-08-03 20:23:02 -0300

    Nimax parte 2

---
+++ cadastro.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>Cadastro</title>
+</head>
+
+<style>
+   body{
+    padding-top:120px;
+    padding-left: 550px;
+    background-image: url(imagem/fundo.jpg);
+    background-attachment: fixed;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-color: white;
+}
+
+</style>
+
+<body>
+
+<form method="post" enctype="multipart/form-data">
+        <div class="container">
+            <div class="card" style="width: 20rem; height: 32rem">
+                <div class="card-body" id="cbody">
+                    <h5 style="text-align: center;">Cadastro<h5>
+                    <div class="input-group mb-2">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                            </svg>
+                        </span>
+                        <input class="form-control" type="text" name="user" placeholder="Usuário">
+                    </div><br>
+                    <div class="input-group mb-2">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-fill" viewBox="0 0 16 16">
+                                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5h16V4H0V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5z"/>
+                            </svg>
+                        </span>
+                        <input class="form-control" type="date" name="data" placeholder="Data de Nascimento">
+                    </div><br>
+                    <div class="input-group mb-2">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
+                                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1h-7zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1h-4z"/>
+                            </svg>
+                        </span>
+                        <input class="form-control" type="text" pattern="\d{3}\.\d{3}\.\d{3}-\d{2}"  name="cpf" placeholder="CPF">
+                    </div><br>
+                    
+                    <div class="input-group mb-2">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                            </svg>
+                        </span>
+                        <input class="form-control" type="number" name="tel" placeholder="Telefone">
+                    </div><br>
+
+                    <div class="input-group mb-2">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+                            </svg>
+                        </span>
+                        <input class="form-control" type="password" name="senha" placeholder="Senha">
+                    </div><br>
+
+                    <div class="input-group mb-2">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+                            </svg>
+                        </span>
+                        <input class="form-control" type="password" name="rsenha" placeholder="Repetir senha">
+                    </div>
+
+                    <div class="d-grid gap-2 d-md-block">
+                      <input class="btn btn-success col-6" type="submit" value="Cadastrar">
+                      <a class="btn btn-primary col-5" href="index.php">Voltar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,14 +9,51 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Nimax</title>
 </head>
+
+<style>
+   body{
+    padding-top:240px;
+    padding-left: 550px;
+    background-image: url(imagem/fundo.jpg);
+    background-attachment: fixed;
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-color: white;
+}
+
+</style>
+
 <body>
     <form method="post" enctype="multipart/form-data">
         <div class="container">
-            <div class="card" style="width: 20rem; height: 11rem">
+            <div class="card" style="width: 20rem; height: 17rem">
                 <div class="card-body" id="cbody">
-                    <input type="text" name="user" placeholder="Usuário"><br><br>
-                    <input type="password" name="senha" placeholder="Senha"><br><br>
-                    <input type="submit" value="Entrar">
+                    <h5 style="text-align: center;">Login<h5>
+                    <div class="input-group mb-2">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                            </svg>
+                        </span>
+                        <input class="form-control" type="text" name="user" placeholder="Usuário">
+                    </div><br>
+                    <div class="input-group mb-2">
+                        <span class="input-group-text" id="basic-addon1">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+                            </svg>
+                        </span>
+                        <input class="form-control" type="password" name="senha" placeholder="Senha">
+                    </div>
+                    <div class="d-grid gap-1 col-12 mx-auto">
+                      <input class="btn btn-danger" type="submit" value="Entrar">
+                    </div>
+                    <p>
+                        <div class="d-grid gap-2 col-12 mx-auto">
+                            <a href="cadastro.php" class="btn btn-success">Cadastrar</a>
+                        </div>
+                    </p>
+                    
                 </div>
             </div>
         </div>
